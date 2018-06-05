d08726f11facfbe755f06092bbd9abbf186841fb
danyhp <dany.panggabean@gmail.com>
2018-06-05 14:42:51 +0700

    Display Recycler View

--- app/src/main/java/com/mmithb/danyhp/loopj/MainActivity.java
+++ app/src/main/java/com/mmithb/danyhp/loopj/MainActivity.java
@@ -2,12 +2,20 @@ package com.mmithb.danyhp.loopj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnLoopjCompleted {
+
+    private RecyclerView recyclerView;
+    private MovieListAdapter movieListAdapter;
+    private LinkedList<Movie> movieLinkedList;
 
     EditText etSearchTerms;
     Button btnSearch;
@@ -26,6 +34,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnSearch.setOnClickListener(this);
 
         myLoopjTask = new MyLoopjTask(this, this);
+
+        movieLinkedList = new LinkedList<>();
+        movieListAdapter = new MovieListAdapter(this, movieLinkedList);
+
+        // RecyclerView and Adapter
+        recyclerView = findViewById(R.id.recyclerview);
+        updateUI();
+
     }
 
     @Override
@@ -33,11 +49,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String searchTerm = etSearchTerms.getText().toString();
         etSearchTerms.setText("");
         // make loopj HTTP call
-        myLoopjTask.executeLoopjCall(searchTerm);
+        movieLinkedList = myLoopjTask.executeLoopjCall(searchTerm);
+        movieListAdapter = new MovieListAdapter(this, movieLinkedList);
+        updateUI();
+        movieListAdapter.notifyDataSetChanged();
+
     }
 
     @Override
     public void taskCompleted(String results) {
         tvSearchResults.setText(results);
+        movieLinkedList.clear();
+    }
+
+    public void updateUI() {
+        if (!movieLinkedList.isEmpty()) {
+            LinearLayoutManager llm = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(llm);
+            recyclerView.setAdapter(movieListAdapter);
+        }
     }
 }
--- app/src/main/java/com/mmithb/danyhp/loopj/MyLoopjTask.java
+++ app/src/main/java/com/mmithb/danyhp/loopj/MyLoopjTask.java
@@ -1,18 +1,24 @@
 package com.mmithb.danyhp.loopj;
 
 import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.LinkedList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class MyLoopjTask {
     public static final String TAG = "MOVIE_TRIVIA";
+    LinkedList<Movie> movieLinkedList;
 
     AsyncHttpClient asyncHttpClient;
     RequestParams requestParams;
@@ -22,25 +28,46 @@ public class MyLoopjTask {
     String API_KEY = "9509b8b5";
     String BASE_URL = "http://www.omdbapi.com/?apikey=" + API_KEY + "&";
     String jsonResponse;
-
-
+    JSONArray jsonArray;
+    boolean jsonResultBoolean;
 
     public MyLoopjTask(Context context, OnLoopjCompleted listener) {
         asyncHttpClient = new AsyncHttpClient();
         requestParams = new RequestParams();
         this.context = context;
         this.loopjListener = listener;
+        movieLinkedList = new LinkedList<Movie>();
     }
 
-    public void executeLoopjCall(String queryTerm) {
+    public LinkedList<Movie> executeLoopjCall(String queryTerm) {
+
         requestParams.put("s", queryTerm);
         asyncHttpClient.get(BASE_URL, requestParams, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
-                jsonResponse = response.toString();
-                loopjListener.taskCompleted(jsonResponse);
-                Log.i(TAG, "onSuccess: " + jsonResponse);
+                try {
+                    jsonResultBoolean = response.getBoolean("Response");
+                    jsonResponse = response.toString();
+                    loopjListener.taskCompleted(jsonResponse);
+                    Log.i(TAG, "onSuccess: " + jsonResponse);
+                    if (jsonResultBoolean) {
+                        jsonArray = response.getJSONArray("Search");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            String movieTitle = object.getString("Title");
+                            String movieYear = object.getString("Year");
+                            String moviePoster = object.getString("Poster");
+                            Movie movie = new Movie(movieTitle, movieYear, moviePoster);
+                            movieLinkedList.add(movie);
+                            Log.d(TAG, movieTitle + " - " + movieYear + " - " + moviePoster);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
@@ -49,5 +76,6 @@ public class MyLoopjTask {
                 Log.e(TAG, "onFailure: " + errorResponse);
             }
         });
+        return movieLinkedList;
     }
 }
